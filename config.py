19696711c33fca47361f816ea5d891b15213591c
bkh1210 <bkh121087@gmail.com>
2026-03-16 21:46:36 +0900

    초기세팅

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+# 프로젝트 폴더 기준으로 .env 경로 설정
+BASE_DIR = Path(__file__).resolve().parent
+ENV_PATH = BASE_DIR / ".env"
 
+# .env 로드
+load_dotenv(dotenv_path=ENV_PATH)
+
+# API KEY 가져오기
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import json
+from prepare_analysis_data import prepare_inputs
 from openai import OpenAI
 from config import OPENAI_API_KEY
-from loader import load_file
+from loader import load_prompt
 
+#prepare_analysis_data에서 가져오기
+full_script, results, audio_metrics, pose = prepare_inputs("audio.json", "pose.json")
 # OpenAI client 생성
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 # 파일 불러오기
-script = load_file("scripts/presentation_script.txt")
-prompt_template = load_file("prompts/edit_prompt.txt")
+prompt_template = load_prompt("prompts/edit_prompt.txt")
 
 # 프롬프트 템플릿에 발표 대본 삽입
-prompt = prompt_template.format(script=script)
+prompt = prompt_template.format(
+    script=full_script,
+    full_script=full_script,
+    results=json.dumps(results, ensure_ascii=False),
+    audio_metrics=json.dumps(audio_metrics, ensure_ascii=False),
+    pose=json.dumps(pose, ensure_ascii=False),
+    analysis_mode="summary"
+)
 
 # LLM 요청
 response = client.chat.completions.create(
